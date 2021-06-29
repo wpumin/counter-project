3b1242889a61452b5214427813b0b0e04851e589
topumin <pumin.wongsiri@gmail.com>
2021-06-29 22:51:40 +0700

    setting up project

--- src/App.js
+++ src/App.js
@@ -3,20 +3,14 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className="app-center">
+        <h1>COUNTER: 1</h1>
+        <div className="panel">
+          <button className="increse" onClick={() => {alert("Coming soon..")}}>+</button>
+          <button className="decrese" onClick={() => {alert("Coming soon..")}}>-</button>
+          <button className="reset" onClick={() => {alert("Coming soon..")}}>RESET</button>
+        </div>
       </header>
     </div>
   );
